d0ea2d5cc7e6f0b183bc40d4f02f44064c88f52b
Anand Srinivasan <77900328+as2110@users.noreply.github.com>
2025-04-16 10:56:40 +0100

    remove duplicate dir

--- src/split_fastqcats/src/split_fastqcats/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-split_fastqcats - A tool for processing and splitting FastQ reads based on primer sequences.
-
-"""
-
-# split_fastqcats/__init__.py
-from .version import __version__
--- src/split_fastqcats/src/split_fastqcats/archived/BH_scripts/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-split_fastqcats - A tool for processing and splitting FastQ reads based on primer sequences
-"""
-
-__version__ = '0.1.0'
-
-from .fastq_splitter import FastqSplitter, main
-
-__all__ = ['FastqSplitter', 'main']
